29383593dca829677d3526ef04ffc1597e4724a2
MottoMottoBR <alessandro.aoc@gmail.com>
2024-03-18 16:09:16 -0400

    Adicionado SQL Lite Local

---
+++ lib/db/db.dart
@@ -0,0 +1,40 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DB {
+  // construto com acesso privado
+  DB._();
+
+  // Criando uma instancia
+  static final DB instance = DB._();
+
+  //Instacia do SQlite
+  get database async {
+    if (database != null) return database;
+    return await _iniDatabase();
+  }
+}
+
+_iniDatabase() async {
+  return await openDatabase(
+    join(await getDatabasesPath(), 'facebookClone.db'),
+    version: 1,
+    onCreate: _onCreate,
+  );
+}
+
+_onCreate(db, versao) async {
+  await db.execute(_nome);
+  await db.execute(_fotoPerfil);
+  await db.execute(_imgPostagem);
+  await db.execute(_textoPostagem);
+}
+
+String get _nome =>
+    ''' CREATE TABLE nome ( id INTEGER PRIMARY KEY AUTOINCREMENTE, nome Usuario ); ''';
+String get _fotoPerfil =>
+    ''' CREATE TABLE nome ( fotoPerfil INTEGER PRIMARY KEY AUTOINCREMENTE, Foto Perfil ); ''';
+String get _imgPostagem =>
+    ''' CREATE TABLE nome ( imgPostagem INTEGER PRIMARY KEY AUTOINCREMENTE, ImG Perfil ); ''';
+String get _textoPostagem =>
+    ''' CREATE TABLE nome ( textoPostagem INTEGER PRIMARY KEY AUTOINCREMENTE, texto Postagem ); ''';
